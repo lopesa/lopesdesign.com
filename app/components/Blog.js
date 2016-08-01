9ff4dd2e9b9c702e4a9189e9d6afda3bd4aa1d55
Tony Lopes <tony@lopesdesign.com>
2016-08-01 14:45:00 -0700

    working blog redirect
    Former-commit-id: ba1b1df5c525273fe14841557972ad99573aa123

--- app/components/Blog.js
+++ app/components/Blog.js
@@ -4,7 +4,7 @@ var ReactRedirect = require('react-redirect');
 var Blog = React.createClass({
   render: function(){
     return(
-      <ReactRedirect location='http://mettalope.com/lopblog'>
+      <ReactRedirect location='/blog'>
       </ReactRedirect>
     )
   }
